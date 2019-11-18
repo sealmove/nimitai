e94f92296da912033b670abc74fbdb45ca923c5a
Stefanos Mandalas <sealmove@protonmail.com>
2019-11-18 18:52:34 +0200

    -

--- ksypeg.nim
+++ ksypeg.nim
@@ -12,35 +12,35 @@ let p = peg "ksy":
   Tag <- '-' * B
   Identifier <- {'a'..'z'} * *{'a'..'z','0'..'9','_'}
   Import <- +{'A'..'Z','a'..'z','0'..'9','_','-','/'}
-  key(item) <- item * B * ':' * B
-  arr4(item) <- +('\n' * ' '[4] * item * B)
-  arr4tagged(item) <- +('\n' * ' '[4] * Tag * item * B)
+  Key(item) <- item * B * ':' * B
+  Arr4(item) <- +('\n' * ' '[4] * item * B)
+  Arr4tagged(item) <- +('\n' * ' '[4] * Tag * item * B)
 
   # Attributes
-  Application <- key("application") * >Any
+  Application <- Key("application") * >Any
   # Consume <-
   # Contents <-
-  Doc <- key("doc") * (('|' * B * *(+'\n' * ' '[2] * Any)) | Any)
-  DocRef <- key("doc-ref") * >Any
-  Encoding <- key("encoding") * >Any
-  Endian <- key("endian") * >("le" | "be") * B
+  Doc <- Key("doc") * (('|' * B * *(+'\n' * ' '[2] * Any)) | Any)
+  DocRef <- Key("doc-ref") * >Any
+  Encoding <- Key("encoding") * >Any
+  Endian <- Key("endian") * >("le" | "be") * B
   # EosError <-
   # enum <-
-  Ext <- key("file-extension") * >(arr4tagged(Any) | Any)
+  Ext <- Key("file-extension") * >(Arr4tagged(Any) | Any)
   # if <-
-  Id <- key("id") * >Identifier
+  Id <- Key("id") * >Identifier
   # Include <-
-  Imports <- key("imports") * >(arr4tagged(Import) | Import)
-  License <- key("license") * >Any
+  Imports <- Key("imports") * >(Arr4tagged(Import) | Import)
+  License <- Key("license") * >Any
   # Repeat <-
   # RepeatExpr <-
   # RepeatUntil <-
   # Process <-
-  Size <- key("size") * Any
+  Size <- Key("size") * Any
   # Size-eos <-
-  Title <- key("title") * Any
+  Title <- Key("title") * Any
   # terminator <-
-  Type <- key("type") * Identifier
+  Type <- Key("type") * Identifier
 
   # Section Attributes
   MetaAttrs <- Id | Title | Application | Ext | License | Imports | Encoding |
@@ -48,14 +48,14 @@ let p = peg "ksy":
   SeqAttrs <- Type | Size
 
   # Sections
-  MetaSec <- key("meta") * '\n' * +(' '[2] * MetaAttrs * '\n'):
+  MetaSec <- Key("meta") * '\n' * +(' '[2] * MetaAttrs * '\n'):
     stdout.write $0
   DocSec <- Doc * '\n':
     stdout.write $0
   DocRefSec <- DocRef:
     stdout.write "docref = " & $0
-  SeqSec <- key("seq") *
-            +('\n' * ' '[2] * Tag * key("id") * >Identifier * arr4(SeqAttrs)):
+  SeqSec <- Key("seq") *
+            +('\n' * ' '[2] * Tag * Key("id") * >Identifier * Arr4(SeqAttrs)):
     stdout.write $0
 
 doAssert p.matchFile("test.ksy").ok
