d4b6ae9d036826f38892c87677367ffa5a6c9614
Stefanos Mandalas <sealmove@protonmail.com>
2019-11-26 02:54:08 +0200

    started implementing codegen

--- nimitai.nim
+++ nimitai.nim
@@ -1,8 +1,67 @@
-import nimitai/private/[ksyast, ksypeg], macros
+import nimitai/private/[ksyast, ksypeg], macros, strutils
 
 macro generateParser*(path: static[string]) =
   let ksy = parseKsy(path)
   result = newStmtList()
 
-  # Generate import statement
-  result.add newNimNode(nnkImportStmt).add(newIdentNode("nimitai"))
+  # Import statement
+  result.add newNimNode(nnkImportStmt).add(
+    newIdentNode("nimitai/private/runtime"))
+
+  # Type section
+  var typesect = newTree(nnkTypeSection)
+  for t in ksy.types:
+    let
+      name = t.name.capitalizeAscii
+      objName = name & "Obj"
+      refDef = nnkTypeDef.newTree(
+        ident(name),
+        newEmptyNode(),
+        nnkRefTy.newTree(ident(objName))
+      )
+    typesect.add(refDef)
+    var
+      objDef = nnkTypeDef.newTree(ident(objName), newEmptyNode())
+      objVal = nnkObjectTy.newTree(newEmptyNode(), newEmptyNode())
+      fields = newTree(nnkRecList)
+    fields.add(
+      nnkIdentDefs.newTree(
+        nnkPostfix.newTree(
+          newIdentNode("*"),
+          newIdentNode("io")
+        ),
+        newIdentNode("KaitaiStream"),
+        newEmptyNode()
+      ),
+      nnkIdentDefs.newTree(
+        nnkPostfix.newTree(
+          newIdentNode("*"),
+          newIdentNode("root")
+        ),
+        newIdentNode(name),
+        newEmptyNode()
+      ),
+      nnkIdentDefs.newTree(
+        nnkPostfix.newTree(
+          newIdentNode("*"),
+          newIdentNode("parent")
+        ),
+        nnkRefTy.newTree(
+          newIdentNode("RootObj")
+        ),
+        newEmptyNode()
+      ),
+      nnkIdentDefs.newTree(
+        nnkPostfix.newTree(
+          newIdentNode("*"),
+          newIdentNode("hello")
+        ),
+        newIdentNode("uint8"),
+        newEmptyNode()
+      )
+    )
+    #for a in t.attrs:
+    objVal.add(fields)
+    objDef.add(objVal)
+    typesect.add(objDef)
+  result.add(typesect)
