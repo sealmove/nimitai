988c22bd6f665af88cc400393a94f2bb2d2fea81
Stefanos Mandalas <sealmove@protonmail.com>
2020-07-14 17:40:53 +0300

    first demo of implementation

--- nimitai.nim
+++ nimitai.nim
@@ -1,8 +1,31 @@
 import macros, json
 
+const
+  rootTypeName = "KaitaiStruct"
+
 proc generateParser(ksj: string): NimNode =
   let json = parseJson(readFile(ksj))
-  result = newLetStmt(ident"x", newLit(json["meta"]["id"]))
+  result = nnkTypeSection.newTree(
+    nnkTypeDef.newTree(
+      ident(json["meta"]["id"].getStr),
+      newEmptyNode(),
+      nnkRefTy.newTree(
+        nnkObjectTy.newTree(
+          newEmptyNode(),
+          nnkOfInherit.newTree(
+            ident(rootTypeName)),
+          nnkRecList.newTree(
+            nnkIdentDefs.newTree(
+              newIdentNode("one"),
+              newIdentNode("uint8"),
+              newEmptyNode()
+            ),
+            nnkIdentDefs.newTree(
+              newIdentNode("parent"),
+              newIdentNode("KaitaiStruct"),
+              newEmptyNode()))))))
+
+  echo repr result
 
 macro injectParser(ksj: static[string]) =
   result = generateParser(ksj)
@@ -11,3 +34,6 @@ proc writeModule(ksj, module: string) =
   writeFile(module, generateParser(ksj).repr)
 
 proc writeDll(ksj, dll: string) = discard
+
+static:
+  discard generateParser("ksj/hello_world.ksj")
