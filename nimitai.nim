a29a8f9ee23d1c8a79e3c9f57172f874553500f2
Stefanos Mandalas <sealmove@protonmail.com>
2020-07-14 20:14:02 +0300

    started implementing typeSection codegen

--- nimitai.nim
+++ nimitai.nim
@@ -3,28 +3,44 @@ import macros, json
 const
   rootTypeName = "KaitaiStruct"
 
+proc typeSection(json: JsonNode): NimNode =
+  result = newTree(nnkTypeSection)
+
+  var fields = newTree(nnkRecList)
+
+  for f in json["seq"]:
+    fields.add(
+      nnkIdentDefs.newTree(
+        ident(f["id"].getStr),
+        ident(f["type"].getStr),
+        newEmptyNode()))
+
+  fields.add(
+    nnkIdentDefs.newTree(
+      newIdentNode("parent"),
+      newIdentNode(rootTypeName),
+      newEmptyNode()))
+
+  result.add(nnkTypeDef.newTree(
+    ident(json["meta"]["id"].getStr),
+    newEmptyNode(),
+    nnkRefTy.newTree(
+      nnkObjectTy.newTree(
+        newEmptyNode(),
+        nnkOfInherit.newTree(
+          ident(rootTypeName)),
+        fields))))
+
+  # debugging
+  echo json.pretty
+
 proc generateParser(ksj: string): NimNode =
   let json = parseJson(readFile(ksj))
-  result = nnkTypeSection.newTree(
-    nnkTypeDef.newTree(
-      ident(json["meta"]["id"].getStr),
-      newEmptyNode(),
-      nnkRefTy.newTree(
-        nnkObjectTy.newTree(
-          newEmptyNode(),
-          nnkOfInherit.newTree(
-            ident(rootTypeName)),
-          nnkRecList.newTree(
-            nnkIdentDefs.newTree(
-              newIdentNode("one"),
-              newIdentNode("uint8"),
-              newEmptyNode()
-            ),
-            nnkIdentDefs.newTree(
-              newIdentNode("parent"),
-              newIdentNode("KaitaiStruct"),
-              newEmptyNode()))))))
 
+  result = newStmtList(
+    typeSection(json))
+
+  # debugging
   echo repr result
 
 macro injectParser(ksj: static[string]) =
@@ -35,5 +51,6 @@ proc writeModule(ksj, module: string) =
 
 proc writeDll(ksj, dll: string) = discard
 
+# debugging
 static:
   discard generateParser("ksj/hello_world.ksj")
