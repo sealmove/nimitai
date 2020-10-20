e43e62b8c7c04c225c792b851f07ec1621e6ecc0
Stefanos Mandalas <sealmove@protonmail.com>
2020-10-20 21:11:54 +0300

    codegen maintainance

--- src/nimitai.nim
+++ src/nimitai.nim
@@ -5,20 +5,20 @@ const
   rootTypeName = "KaitaiStruct"
   streamTypeName = "KaitaiStream"
 
-proc parse(attr: Field, endian: EndianKind): NimNode =
-  if FieldKey.value in attr.keys:
+proc parse(field: Field, endian: EndianKind): NimNode =
+  if FieldKey.value in field.keys:
     return newCall(
-      attr.`type`.parsed.strVal,
-      attr.value)
+      field.`type`.parsed.strVal,
+      field.value)
 
-  if FieldKey.`type` in attr.keys:
-    let t = attr.`type`.raw
+  if FieldKey.`type` in field.keys:
+    let t = field.`type`.raw
     # Number
     if t.match(re"([us][1248]|f[48])(be|le)?"):
       var procName = "read" & t
       if not t.match(re"([us][1])|(.*(be|le))"):
         procName &= $endian
-      result = newCall(procName, attr.io)
+      result = newCall(procName, field.io)
 
     # Bool
     elif t == "b1":
@@ -26,7 +26,7 @@ proc parse(attr: Field, endian: EndianKind): NimNode =
         ident"bool",
         newCall(
           "readBitsIntBe",
-          attr.io,
+          field.io,
           newLit(1)))
 
     # Number from bits
@@ -34,7 +34,7 @@ proc parse(attr: Field, endian: EndianKind): NimNode =
       let bits = parseInt(t[1..^1])
       result = newCall(
         "readBitsIntBe",
-        attr.io,
+        field.io,
         newLit(bits))
 
     # User-defined type
@@ -43,7 +43,7 @@ proc parse(attr: Field, endian: EndianKind): NimNode =
         newDotExpr(
           ident(t.capitalizeAscii),
           ident"read"),
-        attr.io,
+        field.io,
         newDotExpr(
           ident"result",
           ident"root"),
@@ -53,10 +53,10 @@ proc parse(attr: Field, endian: EndianKind): NimNode =
   else:
     result = newCall(
       ident"readBytes",
-      attr.io,
+      field.io,
       newCall(
         ident"int",
-        attr.size))
+        field.size))
 
 proc substream(id, ps, ss, size: NimNode): NimNode =
   result = newStmtList()
@@ -76,100 +76,72 @@ proc substream(id, ps, ss, size: NimNode): NimNode =
         ident"newKaitaiStream",
         id)))
 
-# A series of assignments of parsing calls to local variables or object fields
-proc parseAttr(attr: Field, context: NimNode, endian: EndianKind, postfix = ""): NimNode =
+proc parseField(field: Field, context: NimNode, endian: EndianKind,
+                postfix = ""): NimNode =
   result = newStmtList()
 
-  let id = newDotExpr(context, ident(attr.id & postfix))
+  let id = newDotExpr(context, ident(field.id & postfix))
   var posId: NimNode
 
-  if FieldKey.pos in attr.keys:
-    posId = ident(attr.id & "Pos")
+  if FieldKey.pos in field.keys:
+    posId = ident(field.id & "Pos")
     result.add(
       newLetStmt(
         posId,
         newCall(
           ident"pos",
-          attr.io)))
+          field.io)))
     result.add(
       newCall(
         newDotExpr(
-          attr.io,
+          field.io,
           ident"skip"),
-        attr.pos))
+        field.pos))
 
-  if FieldKey.`size` in attr.keys:
+  if FieldKey.`size` in field.keys:
     let stmts = substream(
-      ident(attr.id & "Raw"),
+      ident(field.id & "Raw"),
       newDotExpr(
         context,
         ident"io"),
-      ident(attr.id & "Io"),
-      attr.size)
+      ident(field.id & "Io"),
+      field.size)
     for s in stmts: result.add(s)
 
-  case attr.repeat
+  case field.repeat
   of none:
     result.add(
       newAssignment(
         id,
-        parse(attr, endian)))
+        parse(field, endian)))
   of eos:
     result.add(
       nnkWhileStmt.newTree(
         prefix(
           newCall(
             ident"eof",
-            attr.io),
+            field.io),
           "not"),
         newCall(
           newDotExpr(id, ident"add"),
-          parse(attr, endian))))
+          parse(field, endian))))
   of expr:
     discard
   of until:
     discard
 
-  if FieldKey.pos in attr.keys:
+  if FieldKey.pos in field.keys:
     result.add(
       newCall(
         newDotExpr(
-          attr.io,
+          field.io,
           ident"seek"),
         posId))
-
-proc instanceProc(inst: Field, node: Type): NimNode =
-  var pa = parseAttr(inst, ident"this", node.meta.endian, postfix = "Inst")
-  pa.add(
-    newAssignment(
-      newDotExpr(
-        ident"this",
-        ident(inst.id & "Cached")),
-      ident"true"))
-
-  result = newProc(
-    ident(inst.id),
-    @[inst.`type`.parsed,
-      newIdentDefs(
-        ident"this",
-        ident(node.id))])
-  result.body = newStmtList(
-    newIfStmt(
-     (prefix(
-        newDotExpr(
-          ident"this",
-          ident(inst.id & "Cached")),
-        "not"),
-      pa)),
-    nnkReturnStmt.newTree(
-      newDotExpr(
-        ident"this",
-        ident(inst.id & "Inst"))))
 
 proc parentType(node: Type): string =
   if node.supertype == nil: rootTypeName else: node.supertype.id
 
-proc typeDecl(node: Type): NimNode =
+proc typeDecl(section: var NimNode, node: Type) =
   var fields = newTree(nnkRecList)
 
   fields.add(
@@ -192,7 +164,7 @@ proc typeDecl(node: Type): NimNode =
         ident(i.id & "Cached"),
         ident"bool"))
 
-  result = nnkTypeDef.newTree(
+  section.add nnkTypeDef.newTree(
     ident(node.id),
     newEmptyNode(),
     nnkRefTy.newTree(
@@ -202,16 +174,11 @@ proc typeDecl(node: Type): NimNode =
           ident(rootTypeName)),
         fields)))
 
-proc typeSection(node: Type): NimNode =
-  result = newTree(nnkTypeSection)
-  result.add(typeDecl(node))
   for t in node.types:
-    result.add(typeDecl(t))
+    typeDecl(section, t)
 
-proc readProc(node: Type): NimNode =
-  result = newProc(name = ident"read")
-
-  result.params = nnkFormalParams.newTree(
+proc readProcParams(node: Type): NimNode =
+  result = nnkFormalParams.newTree(
     ident(node.id),
     newIdentDefs(
       ident"_",
@@ -223,12 +190,48 @@ proc readProc(node: Type): NimNode =
       ident(streamTypeName)),
     newIdentDefs(
       ident"root",
-      ident(rootTypeName),
-      newNilLit()),
+      ident(rootTypeName)),
     newIdentDefs(
       ident"parent",
-      ident(parentType(node)),
-      newNilLit()))
+      ident(parentType(node))))
+
+proc instProcParams(inst: Field, node: Type): NimNode =
+  result = nnkFormalParams.newTree(
+    inst.`type`.parsed,
+    newIdentDefs(
+      ident"this",
+      ident(node.id)))
+
+proc readProcFw(node: Type): NimNode =
+  result = nnkProcDef.newTree(
+    ident"read",
+    newEmptyNode(),
+    newEmptyNode(),
+    readProcParams(node),
+    newEmptyNode(),
+    newEmptyNode(),
+    newEmptyNode())
+
+proc instProcFw(inst: Field, node: Type): NimNode =
+  result = nnkProcDef.newTree(
+    ident(inst.id),
+    newEmptyNode(),
+    newEmptyNode(),
+    instProcParams(inst, node),
+    newEmptyNode(),
+    newEmptyNode(),
+    newEmptyNode())
+
+proc procFwDecl(stmtList: var NimNode, node: Type) =
+  for c in node.types:
+    procFwDecl(stmtList, c)
+  stmtList.add(readProcFw(node))
+  for i in node.instances:
+    stmtList.add(instProcFw(i, node))
+
+proc readProc(node: Type): NimNode =
+  result = newProc(name = ident"read")
+  result.params = readProcParams(node)
 
   var
     constructor = nnkObjConstr.newTree(
@@ -270,42 +273,62 @@ proc readProc(node: Type): NimNode =
           newCall(
             ident"alignToByte",
             newDotExpr(ident"result", ident"io")))
-    let stmts = parseAttr(node.seq[i], ident"result", node.meta.endian)
+    let stmts = parseField(node.seq[i], ident"result", node.meta.endian)
     for s in stmts: result.body.add(s)
 
-proc procs(node: Type): NimNode =
-  result = newStmtList()
+proc instProc(inst: Field, node: Type): NimNode =
+  result = newProc(ident(inst.id))
+  result.params = instProcParams(inst, node)
+
+  var pa = parseField(inst, ident"this", node.meta.endian, postfix = "Inst")
+  pa.add(
+    newAssignment(
+      newDotExpr(
+        ident"this",
+        ident(inst.id & "Cached")),
+      ident"true"))
+
+  result.body = newStmtList(
+    newIfStmt(
+     (prefix(
+        newDotExpr(
+          ident"this",
+          ident(inst.id & "Cached")),
+        "not"),
+      pa)),
+    nnkReturnStmt.newTree(
+      newDotExpr(
+        ident"this",
+        ident(inst.id & "Inst"))))
+
+proc procDecl(stmtList: var NimNode, node: Type) =
   for c in node.types:
-    result.add(procs(c))
+    procDecl(stmtList, c)
+  stmtList.add(readProc(node))
   for i in node.instances:
-    result.add(instanceProc(i, node))
-  result.add(readProc(node))
+    stmtList.add(instProc(i, node))
 
 proc fromFileProc(node: Type): NimNode =
-  newStmtList(
-    nnkProcDef.newTree(
-      ident"fromFile",
-      newEmptyNode(),
-      newEmptyNode(),
-      nnkFormalParams.newTree(
-        ident(node.id),
-        newIdentDefs(
-          ident"_",
-          nnkBracketExpr.newTree(
-            ident"typedesc",
-            ident(node.id))),
-        newIdentDefs(
-          ident"filename",
-          ident"string")),
-      newEmptyNode(),
-      newEmptyNode(),
-      newStmtList(
-        newCall(
-          ident"read",
-          ident(node.id),
-          newCall(
-            ident"newKaitaiFileStream",
-            ident"filename")))))
+  newProc(
+    name = ident"fromFile",
+    params = @[
+      ident(node.id),
+      newIdentDefs(
+        ident"_",
+        nnkBracketExpr.newTree(
+          ident"typedesc",
+          ident(node.id))),
+      newIdentDefs(
+        ident"filename",
+        ident"string")],
+    body = newCall(
+      ident"read",
+      ident(node.id),
+      newCall(
+        ident"newKaitaiFileStream",
+        ident"filename"),
+        newNilLit(),
+        newNilLit()))
 
 proc fromFileProcs(node: Type): NimNode =
   result = newStmtList()
@@ -316,10 +339,22 @@ proc fromFileProcs(node: Type): NimNode =
 
 proc generateParser*(spec: JsonNode): NimNode =
   let spec = spec.toKsType
+
+  var
+    typeSection = newTree(nnkTypeSection)
+    procFwDecls = newStmtList()
+    procDecls = newStmtList()
+
+  typeDecl(typeSection, spec)
+  procFwDecl(procFwDecls, spec)
+  procDecl(procDecls, spec)
+
   result = newStmtList(
-    typeSection(spec),
-    procs(spec),
+    typeSection,
+    procFwDecls,
+    procDecls,
     fromFileProcs(spec))
+
   echo treerepr result
   echo repr result
 
