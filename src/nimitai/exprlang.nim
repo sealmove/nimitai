093235d2edb929c9ead7ae1a41467552f0c504c7
Stefanos Mandalas <sealmove@protonmail.com>
2020-10-23 22:08:37 +0300

    reworked parsing of expression language to use an intermediate AST before producing a Nim AST for greater control

--- src/nimitai/exprlang.nim
+++ src/nimitai/exprlang.nim
@@ -1,41 +1,65 @@
 # Kaitai Struct Expression Language
 
-import parseutils, macros
+import parseutils, macros, strformat
 import strutils except parseBiggestInt
 import npeg
 
 type
-  State = ref object
-    pss: seq[seq[NimNode]]
-    context: string
+  KsNodeKind = enum
+    knkIdx
+    knkArr
+    knkInfix
+    knkMethod
+    knkBool
+    knkInt
+    knkFloat
+    knkStr
+    knkId
+    knkOp
+  KsNode = ref object
+    case kind: KsNodeKind
+    of knkBool:
+      boolval: bool
+    of knkInt:
+      intval: BiggestInt
+    of knkFloat:
+      floatval: float
+    of knkStr, knkId, knkOp:
+      strval: string
+    else:
+      sons: seq[KsNode]
   ParsingError* = object of CatchableError
 
-proc removeLeadingUnderscores(s: var string) =
-  while s[0] == '_':
-    s.delete(0, 0)
+proc add(node: KsNode, children: varargs[KsNode]) =
+  for c in children:
+    node.sons.add(c)
 
-#proc addContext(node: NimNode, context: string) =
-#  for i in 0 ..< node.len:
-#    addContext(node[i], context)
-#    if node[i].kind == nnkDotExpr and node.kind != nnkDotExpr:
-#      node[i][0] = newDotExpr(ident(context), node[i][0])
+proc newKsNode(kind: KsNodeKind, children: varargs[KsNode]): KsNode =
+  doAssert kind in {knkIdx, knkArr, knkInfix, knkMethod}
+  result = KsNode(kind: kind)
+  for c in children:
+    result.add(c)
 
-proc expr*(txt, context: string): NimNode =
-  let p = peg(G, s: State):
+proc `[]`(node: KsNode, index: int): KsNode =
+  doAssert node.kind in {knkIdx, knkArr, knkInfix, knkMethod}
+  node.sons[index]
+
+proc toKs(str: string): KsNode =
+  let p = peg(G, s: seq[seq[KsNode]]):
     # Non-terminal
     G         <- S * expr * !1
     expr      <- S * prefix * *infix
-    prefix    <- tBool | tFloat | tInt | tStr | tId |
-                 idx | arr | parExpr
+    prefix    <- tBool | tFloat | tInt | tStr | tId | idx | arr | parExpr
     idx       <- tId * arrOpen * expr * arrClose:
-      let (idx, id) = (pop(s.pss[^1]), pop(s.pss[^1]))
-      s.pss[^1].add nnkBracketExpr.newTree(id, idx)
+      let (idx, id) = (pop(s[^1]), pop(s[^1]))
+      s[^1].add newKsNode(knkIdx, id, idx)
     arr       <- arrOpen * newLvl * expr * *(',' * expr) * arrClose:
-      let upperLvl = pop(s.pss)
-      var arr = newTree(nnkBracket)
-      for n in upperLvl:
-        arr.add n
-      s.pss[^1].add prefix(arr, "@")
+      let
+        elements = pop(s)
+        newNode = newKsNode(knkArr)
+      for e in elements:
+        newNode.add(e)
+      s[^1].add newNode
     parExpr   <- (parOpen * expr * parClose) ^ 0
     infix     <- >("not")                                 * expr ^  1 |
                  >("or" | "^")                            * expr ^  2 |
@@ -45,55 +69,42 @@ proc expr*(txt, context: string): NimNode =
                  >("+" | "-")                             * expr ^  6 |
                  >("*" | "/")                             * expr ^  7 |
                  >("%")                                   * expr ^  8 |
-                 >(".")                                   * expr ^^ 9:
-      var (r, l) = (pop(s.pss[^1]), pop(s.pss[^1]))
+                 >(".")                                   * expr ^  9:
+      var (r, l) = (pop(s[^1]), pop(s[^1]))
       case $1
       of ".":
-        if l.kind == nnkIdent:
-          l = newDotExpr(ident(s.context), l)
-        s.pss[^1].add newDotExpr(l, r)
+        s[^1].add newKsNode(knkMethod, l, r)
       else:
-        var op: string
-        case $1
-        of "%" : op = "mod"
-        of "<<": op = "shl"
-        of ">>": op = "shr"
-        of "&" : op = "and"
-        of "|" : op = "or"
-        of "^" : op = "xor"
-        else   : op = $1
-        s.pss[^1].add infix(l, op, r)
+        s[^1].add newKsNode(knkInfix, l, KsNode(kind: knkOp, strval: $1), r)
 
     # Terminal
     tBool     <- >("true" | "false") * S:
-      s.pss[^1].add newLit(parseBool($1))
+      s[^1].add KsNode(kind: knkBool, boolval: parseBool($1))
     tFloat    <- >(int * '.' * int * ?('e' * ?{'+', '-'} * int)) * S:
       var f: BiggestFloat
       assert len($1) == parseBiggestFloat($1, f)
-      s.pss[^1].add newFloatLitNode(f)
+      s[^1].add KsNode(kind: knkFloat, floatval: f)
     tInt      <- (tBin | tOct | tHex | tDec) * S
     tBin      <- bin:
       var b: BiggestInt
       assert len($0) == parseBin[BiggestInt]($0, b)
-      s.pss[^1].add newIntLitNode(b)
+      s[^1].add KsNode(kind: knkInt, intval: b)
     tOct      <- oct:
       var o: BiggestInt
       assert len($0) == parseOct[BiggestInt]($0, o)
-      s.pss[^1].add newIntLitNode(o)
+      s[^1].add KsNode(kind: knkInt, intval: o)
     tDec      <- dec:
       var d: BiggestInt
       assert len($0) == parseBiggestInt($0, d)
-      s.pss[^1].add newIntLitNode(d)
+      s[^1].add KsNode(kind: knkInt, intval: d)
     tHex      <- hex:
       var x: BiggestInt
       assert len($0) == parseHex[BiggestInt]($0, x)
-      s.pss[^1].add newIntLitNode(x)
+      s[^1].add KsNode(kind: knkInt, intval: x)
     tStr      <- '\"' * >*(Print - '\"') * '\"' * S:
-      s.pss[^1].add newStrLitNode(($1))
+      s[^1].add KsNode(kind: knkStr, strval: $1)
     tId       <- >((Lower | '_') * *(Alnum | '_')) * S:
-      var id = $1
-      removeLeadingUnderscores(id)
-      s.pss[^1].add ident(id)
+      s[^1].add KsNode(kind: knkId, strval: $1)
 
     # Aux
     S        <- *Space
@@ -110,23 +121,97 @@ proc expr*(txt, context: string): NimNode =
 
     # Aux with side-effects
     newLvl   <- 0:
-      s.pss.add newSeq[NimNode]()
+      s.add newSeq[KsNode]()
 
-  var pss = newSeq[seq[NimNode]]()
-  pss.add newSeq[NimNode]()
-  var state = State(pss: pss, context: context)
-  if not p.match(txt, state).ok:
-    raise newException(ParsingError, "Failed to parse expression: " & txt)
-  if state.pss.len != 1:
-    raise newException(ParsingError, "Stack Length: " & $state.pss.len)
+  var s = newSeq[seq[KsNode]]()
+  s.add newSeq[KsNode]()
+  if not p.match(str, s).ok:
+    raise newException(ParsingError, str)
+  elif s.len == 0:
+    raise newException(ParsingError, str & &" (stacks: {s.len})")
+  elif s[^1].len != 1:
+    raise newException(ParsingError, str & &" (items: {s[0].len})")
+  result = s[^1][0]
 
-  result = state.pss[0][0]
+proc removeLeadingUnderscores(s: var string) =
+  while s[0] == '_':
+    s.delete(0, 0)
+
+proc fixIds(node: var KsNode) =
+  if node.kind in {knkIdx, knkArr, knkInfix, knkMethod}:
+    for i in 0 ..< node.sons.len:
+      fixIds(node.sons[i])
+  elif node.kind == knkId:
+    removeLeadingUnderscores(node.strval)
+
+proc addContext(node: var KsNode, context: string) =
+  node = newKsNode(
+    knkMethod,
+    KsNode(kind: knkId, strval: context),
+    KsNode(kind: knkId, strval: node.strval))
+
+proc addContextRec(node: var KsNode, context: string) =
+  if node.kind in {knkIdx, knkArr, knkInfix}:
+    for i in 0 ..< node.sons.len:
+      if node.sons[i].kind == knkId:
+        node.sons[i].addContext(context)
+      else:
+        node.sons[i].addContextRec(context)
+  if node.kind == knkMethod:
+    if node.sons[0].kind == knkId:
+      node.sons[0].addContext(context)
+    else:
+      node.sons[0].addContextRec(context)
+
+proc toNim(ks: KsNode): NimNode =
+  case ks.kind
+  of knkIdx:
+    result = nnkBracketExpr.newTree(ks[0].toNim, ks[1].toNim)
+  of knkArr:
+    let x = newTree(nnkBracket)
+    for s in ks.sons:
+      x.add s.toNim
+    result = prefix(x, "@")
+  of knkInfix:
+    result = nnkInfix.newTree(ks[1].toNim, ks[0].toNim, ks[2].toNim)
+  of knkBool:
+    result = newLit(ks.boolval)
+  of knkInt:
+    result = newIntLitNode(ks.intval)
+  of knkFloat:
+    result = newFloatLitNode(ks.floatval)
+  of knkStr:
+    result = newStrLitNode(ks.strval)
+  of knkId:
+    result = ident(ks.strval)
+  of knkMethod:
+    result = newDotExpr(ks[0].toNim, ks[1].toNim)
+  of knkOp:
+    var op: string
+    case ks.strval
+    of "%" : op = "mod"
+    of "<<": op = "shl"
+    of ">>": op = "shr"
+    of "&" : op = "and"
+    of "|" : op = "or"
+    of "^" : op = "xor"
+    else   : op = ks.strval
+    result = ident(op)
+
+proc ksAsStrToNim*(str, context: string): NimNode =
+  var ks = str.toKs
+  fixIds(ks)
+  if context != "":
+    ks.addContextRec(context)
+    if ks.kind == knkId:
+      ks.addContext(context)
+  ks.toNim
 
 proc debug(s: string) =
-  let expr = expr(s, "this")
+  let expr = ksAsStrToNim(s, "result")
   echo treeRepr expr
   echo ""
   echo repr expr
   echo ""
 
-#static: debug"a.b + e.f.g * h + 3"
+#static: debug"[0xff]"
